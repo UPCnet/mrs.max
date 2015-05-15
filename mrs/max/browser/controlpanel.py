7eb3c5f18ef5975024afdf82fd8fa89b45bdf52c
Victor Fernandez de Alba <sneridagh@gmail.com>
2015-05-15 12:51:58 +0200

    Config definitive test environm

--- mrs/max/browser/controlpanel.py
+++ mrs/max/browser/controlpanel.py
@@ -18,11 +18,11 @@ from mrs.max import MRSMAXMessageFactory as _
 from plone.directives import form
 
 
-DEFAULT_OAUTH_TOKEN_ENDPOINT = u'https://oauth.beta.upc.edu/devupc'
-DEFAULT_MAX_SERVER = u'https://max.beta.upc.edu/devupc'
+DEFAULT_OAUTH_TOKEN_ENDPOINT = u'https://oauth.beta.upc.edu'
+DEFAULT_MAX_SERVER = u'https://max.beta.upc.edu'
 DEFAULT_HUB_SERVER = u'https://hub.beta.upc.edu'
 # DEFAULT_HUB_SERVER = u'http://hub.upcnet.es'
-DEFAULT_DOMAIN = u'devupc'
+DEFAULT_DOMAIN = u'betaupc'
 DEFAULT_MAX_RESTRICTED_USERNAME = u'restricted'
 
 
