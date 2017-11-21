541abf97be67de17d92d8c1d93ea855fff392c5e
Pilar Marinas <pilar.marinas@upcnet.es>
2017-11-21 15:53:26 +0100

    Back to development: 5.27

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '5.26'
+version = '5.27.dev0'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
