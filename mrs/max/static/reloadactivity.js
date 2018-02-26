44127c7fabe67849c524ab3335c21c0ad8293813
iago.lopez <iago.lopez@upcnet.es>
2018-02-26 17:01:53 +0100

    Añadir un timeout al reloadactivity.js

--- mrs/max/static/reloadactivity.js
+++ mrs/max/static/reloadactivity.js
@@ -1,15 +1,17 @@
-msgCA = max.literals('ca')['new_comment_text'];
-msgES = max.literals('es')['new_comment_text'];
-msgEN = max.literals('en')['new_comment_text'];
-msgCommentBox = [msgCA, msgES, msgEN]
-setInterval(function(){
-  var notWrite = true;
-  $(".maxui-newcommentbox textarea").each(function(){
-    if($(this).val() != "" && !msgCommentBox.includes($(this).val())){
-      notWrite = false;
+setTimeout(function(){
+  msgCA = max.literals('ca')['new_comment_text'];
+  msgES = max.literals('es')['new_comment_text'];
+  msgEN = max.literals('en')['new_comment_text'];
+  msgCommentBox = [msgCA, msgES, msgEN]
+  setInterval(function(){
+    var notWrite = true;
+    $(".maxui-newcommentbox textarea").each(function(){
+      if($(this).val() != "" && !msgCommentBox.includes($(this).val())){
+        notWrite = false;
+      }
+    });
+    if(notWrite){
+      $("#maxui-news-activities .maxui-button").trigger("click");
     }
-  });
-  if(notWrite){
-    $("#maxui-news-activities .maxui-button").trigger("click");
-  }
-}, 180000);
+  }, 180000);
+}, 180000)
