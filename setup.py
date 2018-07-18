33daad99c89f6ff794b8c8d8dfbc9cb5c457673c
Pilar Marinas <pilar.marinas@upcnet.es>
2018-07-18 14:12:58 +0200

    Back to development: 5.34

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '5.33'
+version = '5.34.dev0'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
